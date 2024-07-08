78921012c83eea80aecdb6b8f862b55a25c8b5fc
Raghavendra Malagi <Raghavendramalagi94@gmail.com>
2024-07-08 20:13:28 +0530

    changes to gitignore

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading = React.createElement("h2", { id: "h2" }, "React Rendered h2");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
